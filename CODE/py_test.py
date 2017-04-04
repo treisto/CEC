77251756ce654e0bac6502129b5e517c93d3ae01
Lucian Trestioreanu <treisto111@gmail.com>
2017-04-04 16:42:09 +0200

    updates

--- CODE/py_test.py
+++
@@ -1,4 +0,0 @@
-from cec import *
-
-x=[40.0]*60
-print(cec(x,30,2,1));
--- CODE/setup.py
+++
@@ -1,6 +0,0 @@
-from distutils.core import setup, Extension
-
-# the c++ extension module
-extension_mod = Extension("cec", ["pycec.cpp", "cec17.cpp"])
-
-setup(name = "cec", ext_modules=[extension_mod])
